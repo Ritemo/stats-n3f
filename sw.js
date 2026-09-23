a1d0d9bdf052b082fac7a8c0111637501b8d7900
Ritemo <mathieu.cognard@gmail.com>
2026-09-23 23:23:24 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Change ce numéro à chaque mise à jour de l'appli pour forcer le rafraîchissement
-const VERSION = "n3f-v1";
+const VERSION = "n3f-v2";
 const SHELL = ["./", "index.html", "manifest.json", "icon-192.png", "icon-512.png"];
 
 self.addEventListener("install", e => {
